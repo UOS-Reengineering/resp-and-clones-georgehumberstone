c9d685e2240755b83175c26339f2bb52e767a214
georgehumberstone <83763599+georgehumberstone@users.noreply.github.com>
2023-04-26 11:27:14 +0100

    Task 1

--- src/main/java/example/project/Refactoring/ExtractMethods.java
+++ src/main/java/example/project/Refactoring/ExtractMethods.java
@@ -6,6 +6,22 @@ public class ExtractMethods {
      * This is a method to reengineer using `extract methods` refactoring technique.
      * TODO: complete `foo_after_refactoring()` method below using the refactoring.
      */
+    public static int[] initialiseArray(int size){
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+        }
+        return array;
+
+    }
+    public static void printArrayWithDelimiter(int[] array, String delimiter){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + delimiter);
+        }
+        System.out.println();
+
+    }
+
     public static void foo_before_refactoring() {
         // initialise an array of random numbers with the size of 10
         int[] array1 = new int[10];
@@ -37,16 +53,16 @@ public class ExtractMethods {
         // TODO: uncomment the following lines and complete missing methods
 
 //        // initialise an array of random numbers with the size of 10
-//        int[] array1 = initialiseArray(10);
+        int[] array1 = initialiseArray(10);
 //
 //        // print the array with a given delimiter
-//        printArrayWithDelimiter(array1, " ");
+        printArrayWithDelimiter(array1, " ");
 //
 //        // initialise another array of random numbers with the size of 10
-//        int[] array2 = initialiseArray(10);
+        int[] array2 = initialiseArray(10);
 //
 //        // print the array with a given delimiter
-//        printArrayWithDelimiter(array2, " ");
+        printArrayWithDelimiter(array2, " ");
     }
 
 
